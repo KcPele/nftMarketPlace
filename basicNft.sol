46f73e2f72cf3cb38365d6311c0081e7c059e510
KishoreVB70 <kishore07kzr@gmail.com>
2022-10-25 07:51:20 +0530

    made improvements

--- basicNft.sol
+++ basicNft.sol
@@ -6,18 +6,17 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract BasicNft is ERC721 {
     string public constant TOKEN_URI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
-    uint256 private s_tokenCounter;
+    uint256 private tokenCounter;
 
     event NFTMinted(uint256 indexed tokenId);
 
     constructor() ERC721("Dacade", "DCD") {
-        s_tokenCounter = 0;
     }
 
     function mintNft() public {
-        _safeMint(msg.sender, s_tokenCounter);
-        emit NFTMinted(s_tokenCounter);
-        s_tokenCounter = s_tokenCounter + 1;
+        _safeMint(msg.sender, tokenCounter);
+        emit NFTMinted(tokenCounter);
+        tokenCounter++;
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
@@ -26,6 +25,6 @@ contract BasicNft is ERC721 {
     }
 
     function getTokenCounter() public view returns (uint256) {
-        return s_tokenCounter;
+        return tokenCounter;
     }
 }
